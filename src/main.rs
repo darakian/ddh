bbc8092ee072d36d88ba3bdf7117be2776b6cc47
Jon Moroney <darakian@gmail.com>
2018-12-20 16:10:37 -0800

    Version bump

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use ddh::{Fileinfo, PrintFmt, Verbosity, HashMode};
 
 fn main() {
     let arguments = App::new("Directory Difference hTool")
-                        .version("0.9.8")
+                        .version("0.9.9")
                         .author("Jon Moroney jmoroney@hawaii.edu")
                         .about("Compare and contrast directories.\nExample invocation: ddh /home/jon/downloads /home/jon/documents -p shared")
                         .arg(Arg::with_name("directories")
